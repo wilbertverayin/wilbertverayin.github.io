84a3efe173ba4712e6913597162299b47467320f
Wilbert Verayin <wilbert.verayin@freedom.tm>
2025-07-26 14:13:11 +0800

    fix: new config;

---
+++ app/data/next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // This option enables static exports.
+  output: 'export',
+ 
+  // Optional: Change the default output directory `out` to `dist` or something else.
+  // distDir: 'dist',
+};
+ 
+module.exports = nextConfig;
